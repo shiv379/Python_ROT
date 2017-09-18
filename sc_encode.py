8d67e24541857c70fa1221dc8bce5d0febc8a11d
Shiv379@gmail.com <Shiv379@gmail.com>
2017-09-18 22:56:50 +0100

    Added handling of case

--- sc_encode.py
+++ sc_encode.py
@@ -6,6 +6,9 @@ print(plain_text,"¦")
 plain_alphabet="abcdefghijklmnopqrstuvwxyz"
 encoded_alphabet="nopqrstuvwxyzabcdefghijklm"
 for index in range(len(plain_text)):
-    plain_alphabet_index = plain_alphabet.find(plain_text[index])
-    encoded_text += encoded_alphabet[plain_alphabet_index]
-print("Encoded text: " + encoded_text) 
+    plain_alphabet_index = plain_alphabet.upper().find(plain_text[index].upper())
+    if plain_text[index].isupper():
+        encoded_text += encoded_alphabet[plain_alphabet_index].upper()
+    else:
+        encoded_text += encoded_alphabet[plain_alphabet_index].lower() 
+print("Encoded text: " + encoded_text)
