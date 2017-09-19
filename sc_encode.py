61a8744190c38181d297711450443a3cb295359d
Shiv379@gmail.com <Shiv379@gmail.com>
2017-09-19 07:00:32 +0100

    Moved alphabets to functions

--- sc_encode.py
+++ sc_encode.py
@@ -1,14 +1,20 @@
+def plain_alphabet():
+    return "abcdefghijklmnopqrstuvwxyz" 
+
+def encoded_alphabet():
+    return "nopqrstuvwxyzabcdefghijklm" 
+    
 plain_text = None
 encoded_text = "" 
 while plain_text is None:
     plain_text = input("Enter message to encode: ")
 print(plain_text,"¦")
-plain_alphabet="abcdefghijklmnopqrstuvwxyz"
-encoded_alphabet="nopqrstuvwxyzabcdefghijklm"
+p_alpha=plain_alphabet() 
+e_alpha=encoded_alphabet() 
 for index in range(len(plain_text)):
-    plain_alphabet_index = plain_alphabet.upper().find(plain_text[index].upper())
+    plain_alphabet_index = p_alpha.upper().find(plain_text[index].upper())
     if plain_text[index].isupper():
-        encoded_text += encoded_alphabet[plain_alphabet_index].upper()
+        encoded_text += e_alpha[plain_alphabet_index].upper()
     else:
-        encoded_text += encoded_alphabet[plain_alphabet_index].lower() 
+        encoded_text += e_alpha[plain_alphabet_index].lower() 
 print("Encoded text: " + encoded_text)
