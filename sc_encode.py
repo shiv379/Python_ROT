3256e207dd75c47787eeb31041388031c8abca35
Shiv379@gmail.com <Shiv379@gmail.com>
2017-09-20 17:47:06 +0100

    Created alphabet shifting function

--- sc_encode.py
+++ sc_encode.py
@@ -1,16 +1,24 @@
+import string
+
 def plain_alphabet():
-    return "abcdefghijklmnopqrstuvwxyz" 
+    return string.ascii_lowercase
 
 def encoded_alphabet():
     return "nopqrstuvwxyzabcdefghijklm" 
     
+def shifted_alphabet(shift):
+    shifted = "" 
+    for i in string.ascii_lowercase:
+        shifted += string.ascii_lowercase[(string.ascii_lowercase.index(i) + shift) % 26]
+    return shifted
+
 plain_text = None
-encoded_text = "" 
+encoded_text = ""
 while plain_text is None:
-    plain_text = input("Enter message to encode: ")
+    plain_text = input("Enter message to encode: ").strip()
 print(plain_text,"¦")
-p_alpha=plain_alphabet() 
-e_alpha=encoded_alphabet() 
+p_alpha=shifted_alphabet(0) 
+e_alpha=shifted_alphabet(3) 
 for index in range(len(plain_text)):
     plain_alphabet_index = p_alpha.upper().find(plain_text[index].upper())
     if plain_text[index].isupper():
