92b14b54ebd4fccdc9b9b1b5fa27ac0b3a32a64c
Shiv379@gmail.com <Shiv379@gmail.com>
2017-09-23 23:48:36 +0100

    Updated shifted_alphabet() to use collections

--- sc_encode.py
+++ sc_encode.py
@@ -1,9 +1,10 @@
 import string
+import os
+import collections
 
 def shifted_alphabet(shift):
-    shifted = "" 
-    for i in string.ascii_lowercase:
-        shifted += string.ascii_lowercase[(string.ascii_lowercase.index(i) + shift) % 26]
+    shifted = collections.deque(string.ascii_lowercase) 
+    shifted.rotate(shift)
     return shifted
 
 def decode(plain_text, shift):
@@ -22,10 +23,36 @@ def decode(plain_text, shift):
 
 def brute_force(plain_text):
     for i in range(0,26):
-        print(decode(plain_text, i)) 
+        print(decode(plain_text, i))
+    input("\n Press any key.") 
 
-plain_text = None
-while plain_text is None:
-    plain_text = input("Enter message to encode: ").strip() 
-#print(decode(plain_text, 1))
-brute_force(plain_text) 
+def menu():
+    global plain_text
+    menu = {}
+    menu['1']="Enter text" 
+    menu['2']="Fixed shift"
+    menu['3']="Brute force"
+    menu['4']="Exit"
+    while True: 
+      os.system('clear') 
+      options=sorted(menu.keys()) 
+      print('{:*^40}'.format('ROT Decoder'),"\n\n") 
+      print("Input text: ",plain_text,"\n\n") 
+      for entry in options: 
+          print(entry, menu[entry])
+
+      selection=input("\nPlease Select: ") 
+      if selection =='1': 
+          plain_text = None
+          while plain_text is None:
+              plain_text = input("\nEnter message to encode: ").strip() 
+      elif selection == '2': 
+          
+          decode(plain_text, 1)
+      elif selection == '3':
+          brute_force(plain_text) 
+      elif selection == '4': 
+          break
+      
+plain_text =None
+menu() 
