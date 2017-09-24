e6d0128adbd64df41bb5fb990e326aa8d28046ab
Shiv379@gmail.com <Shiv379@gmail.com>
2017-09-24 22:34:42 +0100

    Improved menu, hidden non - valid options

--- sc_encode.py
+++ sc_encode.py
@@ -9,44 +9,54 @@ def shifted_alphabet(shift):
 def decode(plain_text, shift):
     e_alpha=shifted_alphabet(shift) 
     encoded_text = ""
-    trans_table = "".maketrans(string.ascii_lowercase, e_alpha) 
+    trans_table = "".maketrans(string.ascii_lowercase + string.ascii_uppercase, e_alpha+e_alpha.upper()) 
     return plain_text.translate(trans_table)
 
 def brute_force(plain_text):
     for i in range(0,26):
-        print(decode(plain_text, i))
-    input("\n Press any key.") 
+        print(i, decode(plain_text, i))
+    input() 
 
 def menu():
     global plain_text
-    selected_menu=98
-    menu = {}
-    menu['1']="Enter text" 
-    menu['2']="Fixed shift"
-    menu['3']="Brute force"
-    menu['4']="Exit"
-    while True: 
-      os.system('clear') 
-      options=sorted(menu.keys()) 
-      print('{:*^40}'.format('ROT Decoder'),"\n\n") 
-      print("Input text: ",plain_text,"\n\n") 
-      for entry in options: 
-          #print(entry, int(entry) ==selected_menu) 
-          print("*"*(int(entry)==selected_menu)," "*(int(entry)!=selected_menu), entry, menu[entry])
-
-      #selection=input("\nPlease Select: ") 
-      selection=getKey() 
-      if selection =='1': 
-          plain_text = None
-          while plain_text is None:
-              plain_text = input("\nEnter message to encode: ").strip() 
-      elif selection == '2': 
-          print(decode(plain_text, 1)) 
-          input() 
-      elif selection == '3':
-          brute_force(plain_text) 
-      elif selection == '4': 
-          break
+    while True:
+        menu = {}
+        menu['1']="Enter text" 
+        menu['9']="Exit"
+        if plain_text is not None:
+            menu['2']="Fixed shift"
+            menu['3']="Brute force"
+            menu['4']="Output to file: OFF" 
+        os.system('clear') 
+        options=sorted(menu.keys()) 
+        print('{:*^40}'.format('ROT Decoder'),"\n\n") 
+        print("Input text: ",plain_text,"\n\n") 
+        for entry in options: 
+            print(entry, menu[entry])
+        print("") 
+        selection=getKey() 
+        if selection not in options:
+            print() 
+        elif selection =='1': #Enter text
+            plain_text = None
+            while plain_text is None:
+                plain_text = input("\nEnter message to encode: ").strip() 
+        elif selection == '2': #Fixed shift
+            shift = None
+            while not isinstance(shift, int):
+                try:
+                    shift=int(input("Shift: "))
+                except ValueError:
+                    print("Please enter a valid whole number.") 
+            print("\n", decode(plain_text, shift)) 
+            getKey() 
+        elif selection == '3': #Brute force
+            brute_force(plain_text) 
+        elif selection == '4': #File out
+            print("Not implemented yet\nPress any key") 
+            getKey() 
+        elif selection == '9': #Exit
+            break
       
-plain_text ="abcdef"
+plain_text = None
 menu() 
